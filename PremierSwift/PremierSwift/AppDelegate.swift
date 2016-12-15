68e8fac28fab9303fddb1675b7801d36c2abb1ed
ceeK <chris.kevin.howell@gmail.com>
2016-12-15 19:17:20 +0000

    Alter AppDelegate to guard against creating a window.

--- PremierSwift/PremierSwift/AppDelegate.swift
+++ PremierSwift/PremierSwift/AppDelegate.swift
@@ -7,10 +7,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
+        
+        guard let window = window else { return false }
         let moviesViewController = MoviesViewController()
         let navigationController = UINavigationController(rootViewController: moviesViewController)
-        window!.rootViewController = navigationController
-        window!.makeKeyAndVisible()
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
         return true
     }
 }
